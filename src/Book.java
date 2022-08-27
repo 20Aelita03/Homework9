043e313976b32eb2f8aede4a7d51fcd11d04c856
aires <airesheva@mail.ru>
2022-08-27 17:32:28 +0300

    Second commit1

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,25 @@
 public class Book {
+    String bookName;
+    Author author;
+    int yearPublic;
 
+
+
+    public Book(String bookName, Author author, int yearPublic) {
+        this.bookName = bookName;
+        this.author = author;
+        this.yearPublic = yearPublic;
+    }
+    public String getBookName() {
+        return bookName;
+    }
+    public Author getAuthor() {
+        return author;
+    }
+    public int getYearPublic() {
+        return yearPublic;
+    }
+    public void setYearPublic( int yearPublic) {
+        this.yearPublic = yearPublic;
+    }
 }
