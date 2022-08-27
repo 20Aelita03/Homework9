7746d8e8708ba27a667ef19084febec64624523f
aires <airesheva@mail.ru>
2022-08-27 17:27:12 +0300

    Second commit1

--- src/Author.java
+++ src/Author.java
@@ -1,16 +1,3 @@
 public class Author {
-    String authorName;
-    String authorSecondName;
 
-    public Author(String authorName, String authorSecondName){
-        this.authorName = authorName;
-        this.authorSecondName = authorSecondName;
-    }
-
-    public String getAuthorName() {
-        return authorName;
-    }
-    public String getAuthorSecondName() {
-        return authorSecondName;
-    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,3 @@
 public class Main {
-    public static void main(String[] args) {
-        Author author1 = new Author( "Stephen", "King");
-        Author author2 = new Author("Jack", "London");
 
-        Book book1 = new Book("Pet cemetery", author1, 2000);
-        Book book2 = new Book("White Fang", author2, 1998);
-
-        book1.setYearPublic(2012);
-        System.out.println(book1.getAuthor().getAuthorName());
-        System.out.println(book1.getYearPublic());
-    }
 }
