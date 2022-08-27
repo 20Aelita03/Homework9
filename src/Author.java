dc09cbdb45382a6232fb1af46f7ffeb6b4d90921
aires <airesheva@mail.ru>
2022-08-27 17:23:44 +0300

    Second commit

---
+++ src/Author.java
@@ -0,0 +1,16 @@
+public class Author {
+    String authorName;
+    String authorSecondName;
+
+    public Author(String authorName, String authorSecondName){
+        this.authorName = authorName;
+        this.authorSecondName = authorSecondName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+    public String getAuthorSecondName() {
+        return authorSecondName;
+    }
+}
